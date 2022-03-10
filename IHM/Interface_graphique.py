b858ed64885ae94b43d8c30800f13b459bdababd
AARIZOU Hamza Atmane <AARIZOU@0440021j.paysdelaloire.education>
2022-03-10 15:35:13 +0100

    mort

--- IHM/Interface_graphique.py
+++ IHM/Interface_graphique.py
@@ -1,4 +1,5 @@
 import pygame
+import math
 pygame.init
 
 class Game:
@@ -15,25 +16,35 @@ screen = pygame.display.set_mode((1200, 700)) #Définir la taille de la fenêtre
 
 
 # Ajouter un fond
-background = pygame.image.load("C:/Users/Kratoss/Desktop/PygameProject/assets/fond_choipxeau.jpg")
+background = pygame.image.load("fond_choipxeau.jpg")
 background = pygame.transform.scale(background, (1200, 850))
 
 # Ajouter une bannière pour la page d'accueil
-banner = pygame.image.load("C:/Users/Kratoss/Desktop/PygameProject/assets/banner.jpg")
+banner = pygame.image.load("banner.jpg")
 banner = pygame.transform.scale(banner, (1200, 800))
 
 #Ajouter les boutons
-bouton_play = pygame.image.load("C:/Users/Kratoss/Desktop/PygameProject/assets/button.png")
+bouton_play = pygame.image.load("button.png")
 bouton_play = pygame.transform.scale(bouton_play, (350, 150))
+bouton_play_rect = bouton_play.get_rect()
+
+
+bouton_quit = pygame.image.load("quit.png")
+bouton_quit = pygame.transform.scale(bouton_quit, (350, 150))
+bouton_quit_rect = bouton_quit.get_rect()
+
+
 # Laisser la fenêtre ouverte
 running = True 
 while running:
     if game.jeu_lancé:
         screen.blit(background, (0, -100))
+        screen.blit(bouton_quit, (0, 500))
     else:
         screen.blit(banner, (0, -100))
-        screen.blit(bouton_play, (420, 250))
+        screen.blit(bouton_play, (0, 0))
 
+       
     # Mettre a jour la fenêtre
     pygame.display.flip()
 
@@ -45,5 +56,16 @@ while running:
             pygame.quit
             # Jeu fermé
             print("Vous avez fermé le jeu")
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            mouse = pygame.mouse.get_pos()
+            if bouton_play_rect.collidepoint(mouse[0], mouse[1]):
+                game.jeu_lancé = True
+   
+            if bouton_quit_rect.collidepoint(mouse[0], mouse[1]):
+                game.jeu_lancé = False
+                print("quit")
+              
+    
+
 
 
