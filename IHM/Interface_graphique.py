bd07be8a3a01594f3a851b1ec87ed6f9e89dbbb9
Ryyugaaa <96998688+Ryyugaa@users.noreply.github.com>
2022-03-10 21:32:07 +0100

    ca avance

--- IHM/Interface_graphique.py
+++ IHM/Interface_graphique.py
@@ -1,6 +1,8 @@
+import py
 import pygame
 import math
-pygame.init
+pygame.init()
+
 
 class Game:
     def __init__(self):
@@ -34,12 +36,39 @@ bouton_quit = pygame.transform.scale(bouton_quit, (350, 150))
 bouton_quit_rect = bouton_quit.get_rect()
 
 
+bouton_perso = pygame.image.load("bouton_perso.png")
+bouton_perso = pygame.transform.scale(bouton_perso, (400, 120))
+bouton_perso_rect = bouton_perso.get_rect()
+
+bouton_manuel = pygame.image.load("perso_manuel.png")
+bouton_manuel = pygame.transform.scale(bouton_manuel, (400, 120))
+bouton_manuel_rect = bouton_manuel.get_rect()
+
+
+#Definir la police d'écriture
+police = pygame.font.Font("harryfont.ttf", 70)
+
+
 # Laisser la fenêtre ouverte
 running = True 
 while running:
     if game.jeu_lancé:
         screen.blit(background, (0, -100))
-        screen.blit(bouton_quit, (0, 500))
+        screen.blit(bouton_quit, (0, 0))
+        for event in pygame.event.get():
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                mouse = pygame.mouse.get_pos()
+                if bouton_quit_rect.collidepoint(mouse[0], mouse[1]):
+                    game.jeu_lancé = False
+        screen.blit(bouton_perso, (400, 150)) 
+        screen.blit(bouton_manuel, (400, 500))
+        for event in pygame.event.get():
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                mouse = pygame.mouse.get_pos()
+                if bouton_perso_rect.collidepoint(mouse[0], mouse[1]):
+                    print("A FINIR VENDREDI 11")
+
+
     else:
         screen.blit(banner, (0, -100))
         screen.blit(bouton_play, (0, 0))
@@ -60,10 +89,9 @@ while running:
             mouse = pygame.mouse.get_pos()
             if bouton_play_rect.collidepoint(mouse[0], mouse[1]):
                 game.jeu_lancé = True
+                
    
-            if bouton_quit_rect.collidepoint(mouse[0], mouse[1]):
-                game.jeu_lancé = False
-                print("quit")
+          
               
     
 
